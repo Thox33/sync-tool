1b40347f515b9d38cdd639daf8199b0bcffd6361
Pascal Helmer <pascal@pascalhelmer.de>
2024-05-06 14:30:48 +0200

    fix: Azure DevOps mapping includes dots in keys

--- sync_tool/core/data/mapping/mapping_helper.py
+++ sync_tool/core/data/mapping/mapping_helper.py
@@ -2,12 +2,24 @@ from typing import Any, Dict
 
 
 def get_field_data_by_path(data: Dict[str, Any], path: str) -> Any:
-    """Get the data from a given path in a (possible) nested dict."""
+    """Get the data from a given path in a (possible) nested dict.
+    Will allow path to include dots inside of the keys by wrapping the part of the key inside of square brackets.
+    """
+
     keys = path.split(".")
     current_data = data
 
     try:
+        concat_key = ""  # This will be used to store the key with dots inside of it
         for key in keys:
+            print(key, concat_key)
+            if "[" in key and "]" not in key:
+                concat_key += key + "."
+                continue
+            elif "]" in key:
+                concat_key += key
+                key = concat_key.replace("[", "").replace("]", "")
+                concat_key = ""
             current_data = current_data[key]
 
         return current_data
--- tests/core/data/mapping/test_mapping_helper.py
+++ tests/core/data/mapping/test_mapping_helper.py
@@ -29,3 +29,21 @@ def test_get_field_data_by_path_empty_path():
     data = {"key1": "value"}
     path = ""
     assert get_field_data_by_path(data, path) is None
+
+
+def test_get_field_data_by_path_with_dots_in_keys():
+    data = {"key1.key2": "value"}
+    path = "[key1.key2]"
+    assert get_field_data_by_path(data, path) == "value"
+
+
+def test_get_field_data_by_path_with_dots_in_keys_nested():
+    data = {"other": {"key1.key2": "value"}}
+    path = "other.[key1.key2]"
+    assert get_field_data_by_path(data, path) == "value"
+
+
+def test_get_field_data_by_path_with_dots_in_keys_nested_alternative():
+    data = {"key1.key2": {"other": "value"}}
+    path = "[key1.key2].other"
+    assert get_field_data_by_path(data, path) == "value"
