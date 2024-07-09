9be92197354d72fb43a66daf2e97b209a2f5fd23
Pascal Helmer <pascal@pascalhelmer.de>
2024-07-09 17:01:34 +0200

    fix: import error

--- sync_tool/core/types/__init__.py
+++ sync_tool/core/types/__init__.py
@@ -1,4 +1,4 @@
-from sync_tool.core.types.field_type import RichTextValue, FieldTypeSyncStatus
+from sync_tool.core.types.field_type import RichTextValue, SyncStatusValue
 from sync_tool.core.types.internal_type import InternalType, create_internal_type
 
-__all__ = ["InternalType", "create_internal_type", "RichTextValue", "FieldTypeSyncStatus"]
+__all__ = ["InternalType", "create_internal_type", "RichTextValue", "SyncStatusValue"]
